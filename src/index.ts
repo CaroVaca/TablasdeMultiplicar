c1208a31e38990b759245ba2500d1c23ccfc80f8
CaroVaca <104107454+CaroVaca@users.noreply.github.com>
2022-05-16 18:29:54 -0300

    cambie unas comillas por suerte ahora nada bien

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
-let btnEnviar=document.getElementById("Enviar");
-let dato1=document.getElementById("dato1");
-let dato2=document.getElementById("dato2");
+let btnEnviar = document.getElementById("Enviar");
+let rotulo1 = document.getElementById("rotulo1");
+let rotulo2 = document.getElementById("rotulo2");
+let dato1 = document.getElementById("dato1");
+let dato2 = document.getElementById("dato2");
 
-rotulo1.innerHTML= “¿La tabla de qué número desea?";
-rotulo2.innerHTML= “¿Hasta qué número desea?";
+rotulo1.innerHTML = "¿La tabla de qué número desea?";
+rotulo2.innerHTML = "¿Hasta qué número desea?";
 
-btnEnviar.addEventListener ("click",()=>{
-let numTabla : number = Number(dato1.value);
-let limiteTabla : number = Number(dato2.value);
-let resultado : number = 0;
-for ( let contador : number = 1; contador <= limiteTabla; contador++) {
-   resultado = numTabla * contador;
-   console.log(numTabla, " x ", contador, " = ", resultado );
-}
+btnEnviar.addEventListener("click", () => {
+  let numTabla: number = Number(dato1.value);
+  let limiteTabla: number = Number(dato2.value);
+  let resultado: number = 0;
+  for (let contador: number = 1; contador <= limiteTabla; contador++) {
+    resultado = numTabla * contador;
+    console.log(numTabla + " x " + contador + " = " + resultado);
+  }
 });
